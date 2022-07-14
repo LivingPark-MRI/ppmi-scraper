d3390063f4dfa16017c955bdd19c50f7b084e004
Tristan Glatard <tristan.glatard@concordia.ca>
2022-07-14 14:52:53 -0400

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DEPS = [ 'webdriver_manager', 'selenium']
 
 setup(
     name='ppmi_downloader',
-    version='0.2.1',
+    version='0.2.2',
     description='A downloader of PPMI files.',
     author='Tristan Glatard',
     author_email='tristan.glatard@concordia.ca',
