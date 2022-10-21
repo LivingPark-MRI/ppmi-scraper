693c17c9b63b61fea63481dc7bd799ea80f36724
Yohan Chatelain <yohan.chatelain@gmail.com>
2022-10-21 09:08:43 -0400

    WIP

---
+++ ppmi_downloader/ppmi_logger.py
@@ -0,0 +1,32 @@
+import icecream
+import sys
+from enum import IntEnum, auto
+
+
+class LoggingLevel(IntEnum):
+    DEBUG = auto()
+    INFO = auto()
+    WARNING = auto()
+    ERROR = auto()
+
+
+loggin_level = LoggingLevel.DEBUG
+
+
+def icecream_factory(logging_class,
+                     at_exit=lambda: (),
+                     prefix='LivingPark-utils'):
+    if logging_class < loggin_level:
+        return lambda msg: ()
+
+    def output_function(s):
+        icecream.colorizedStderrPrint(s)
+        at_exit()
+    return icecream.IceCreamDebugger(prefix=f'{prefix}|{logging_class.name}',
+                                     outputFunction=output_function)
+
+
+debug = icecream_factory(LoggingLevel.DEBUG)
+info = icecream_factory(LoggingLevel.INFO)
+warning = icecream_factory(LoggingLevel.WARNING)
+error = icecream_factory(LoggingLevel.ERROR, at_exit=lambda: sys.exit(1))
--- ppmi_downloader/tests/test_ppmi_downloader.py
+++ ppmi_downloader/tests/test_ppmi_downloader.py
@@ -1,5 +1,9 @@
-import json
+from functools import cache
 import os
+import tempfile
+from tempfile import tempdir
+import livingpark_utils
+import json
 from pathlib import Path
 import random
 
@@ -19,6 +23,28 @@ def test_download_metadata():
     ppmi.download_metadata(random.sample(filenames, min(3, len(filenames))))
 
 
+def test_download_ppmi_metadata():
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        utils = livingpark_utils.LivingParkUtils(tmpdir)
+
+        required_files = [
+            "Demographics.csv",
+            "REM_Sleep_Behavior_Disorder_Questionnaire.csv",
+            "Primary_Clinical_Diagnosis.csv",
+            "Cognitive_Categorization.csv",
+            "Medical_Conditions_Log.csv",
+            "Concomitant_Medication_Log.csv",
+            "Prodromal_History.csv",
+        ]
+
+        utils.download_ppmi_metadata(required_files, headless=False)
+
+
+def test_ppmi_login():
+    ppmi.init_and_log(headless=True)
+
+
 @pytest.mark.flaky(reruns=3, reruns_delay=5)
 def test_download_3D_T1_info():
     ppmi.download_3D_T1_info()
@@ -28,3 +54,15 @@ def test_download_3D_T1_info():
 def test_download_imaging_data():
     ppmi.download_imaging_data([3001, 3003, 3011])
     ppmi.download_imaging_data([3001, 3003, 3011], type="nifti")
+
+
+def test_crawl_study_data():
+    cache_file = 'study_data_to_checkbox_id.csv'
+    ppmi.crawl_study_data(cache_file=cache_file)
+    assert os.path.exists(cache_file)
+
+
+def test_crawl_advanced_search():
+    cache_file = 'search_to_checkbox_id.csv'
+    ppmi.crawl_advanced_search(cache_file=cache_file)
+    assert os.path.exists(cache_file)
