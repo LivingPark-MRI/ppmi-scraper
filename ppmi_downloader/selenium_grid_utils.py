a6c7ee3424b9943f83353e5978953744bd8e74e0
Yohan Chatelain <yohan.chatelain@gmail.com>
2022-11-02 08:44:31 -0400

    Updates for running with selenium grid: 	- Add scripts to build and run singularity container 	with Selenium-Grid manager 	- Add remote option to PPMIDownloader to specify url to listen on 	- Initialization of driver and html is done in PPMIDownloader __init__ 	- Update PPMIDownloader to be robust to existing session 	  * Cookie already accepted 	  * User already logged in 	- Modify donwload_complete functions in download_* functions: 	  * Remove some assert that expect a certain number of files (to fix in future) 	- Takes a screenshot if an error is raised during the execution

---
+++ ppmi_downloader/selenium_grid_utils.py
@@ -0,0 +1,108 @@
+import sys
+from pathlib import Path
+import os
+
+from spython.main import Client
+
+VERBOSE_DEFAULT = False
+SELENIUM_VERSION_DEFAULT = "4.5.0"
+SINGULARITY_CACHE_FOLDER_DEFAULT = '.ppmi_singularity_cache'
+SINGULARITY_BUILD_LOG_DEFAULT = 'ppmi-singularity-build.log'
+SINGULARITY_RUN_LOG_DEFAULT = 'ppmi-singularity-run.log'
+
+env_args = {
+    'PPMI_SINGULARITY_SELENIUM_VERSION': SELENIUM_VERSION_DEFAULT,
+    'PPMI_SINGULARITY_BUILD_CACHE': SINGULARITY_CACHE_FOLDER_DEFAULT,
+    'PPMI_SINGULARITY_BUILD_VERBOSE': VERBOSE_DEFAULT,
+    'PPMI_SINGULARITY_RUN_VERBOSE': VERBOSE_DEFAULT,
+    'PPMI_SINGULARITY_BUILD_LOG': SINGULARITY_BUILD_LOG_DEFAULT,
+    'PPMI_SINGULARITY_RUN_LOG': SINGULARITY_RUN_LOG_DEFAULT,
+}
+
+
+def parse_kwargs():
+    kwargs = {}
+    for env, default in env_args.items():
+        kwargs[env] = os.getenv(env, default=default)
+    print(kwargs)
+    return {'version': kwargs['PPMI_SINGULARITY_SELENIUM_VERSION'],
+            'cache': kwargs['PPMI_SINGULARITY_BUILD_CACHE'],
+            'build_verbose': kwargs['PPMI_SINGULARITY_BUILD_VERBOSE'],
+            'run_verbose': kwargs['PPMI_SINGULARITY_RUN_VERBOSE'],
+            'build_log': kwargs['PPMI_SINGULARITY_BUILD_LOG'],
+            'run_log': kwargs['PPMI_SINGULARITY_RUN_LOG']
+            }
+
+
+def get_image_recipe(version):
+    return f'docker://selenium/standalone-chrome:{version}'
+
+
+def get_image_name(version):
+    return f'selenium-standalone-chrome-{version}.sif'
+
+
+def build():
+    kwargs = parse_kwargs()
+    cache = kwargs['cache']
+    version = kwargs['version']
+    verbose = kwargs['build_verbose']
+    log = kwargs['build_log']
+
+    image = get_image_name(version)
+    recipe = get_image_recipe(version)
+    os.makedirs(kwargs['cache'], exist_ok=True)
+    image, stream = Client.build(recipe=recipe,
+                                 image=image,
+                                 stream=True,
+                                 build_folder=cache,
+                                 sudo=False)
+
+    with open(log, 'w', encoding='utf-8') as fo:
+        for line in stream:
+            print(line, sep='', file=fo)
+            if verbose:
+                print(line, sep='')
+
+    sys.exit()
+
+
+def run():
+
+    kwargs = parse_kwargs()
+    cache = kwargs['cache']
+    version = kwargs['version']
+    verbose = kwargs['run_verbose']
+    log = kwargs['run_log']
+
+    image_name = get_image_name(version)
+    image = os.path.join(cache, image_name)
+
+    Client.load(image)
+
+    Path('supervisord.log').touch()
+    Path('config.toml').touch()
+    Path('supervisord.pid').touch()
+
+    pwd = os.getcwd()
+    bindings = [
+        f'{pwd}/supervisord.log:/var/log/supervisor/supervisord.log',
+        f'{pwd}/config.toml:/opt/selenium/config.toml',
+        f'{pwd}/supervisord.pid:/var/run/supervisor/supervisord.pid'
+    ]
+
+    options = ['--network-args="mapport=4444:4444/tcp"']
+    runner = Client.run(image=image,
+                        stream=True,
+                        bind=bindings,
+                        options=options)
+
+    with open(log, 'w', encoding='utf-8') as fo:
+        for line in runner:
+            print(line, end='', file=fo)
+            if verbose:
+                print(line, end='')
+            fo.flush()
+            sys.stdout.flush()
+
+    sys.exit()
